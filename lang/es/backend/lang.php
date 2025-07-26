00e6faaaf49417bcfc78ae0f30aff461099f304c
Héctor Cáceres <hcaceres@opentech.com.py>
2025-07-26 01:20:40 -0300

    Adding products and blog components.

---
+++ lang/es/backend/lang.php
@@ -0,0 +1,8 @@
+<?php return [
+    'form' => [
+        'edit' => 'Editar datos',
+        'field_on' => 'Sí',
+        'field_off' => 'No',
+        'change_status' => 'Forzar aprobación',
+    ],
+];
