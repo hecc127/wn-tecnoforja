5b6ce9c8a366e86c6412d303ff5d6ceed0d45c3c
Héctor Cáceres <hcaceres@opentech.com.py>
2025-07-27 16:01:26 -0300

    Add products on template.

--- plugins/workteam/core/Plugin.php
+++ plugins/workteam/core/Plugin.php
@@ -1,6 +1,9 @@
 <?php namespace Workteam\Core;
 
+use Winter\Blog\Models\Post;
 use System\Classes\PluginBase;
+use Workteam\Shoppers\Models\Product;
+use Workteam\Webweaver\Models\Page;
 
 class Plugin extends PluginBase
 {
@@ -10,5 +13,42 @@ class Plugin extends PluginBase
 
     public function registerSettings()
     {
+    }
+
+    public function boot()
+    {
+        $this->_extendPage();
+    }
+
+    private function _extendPage()
+    {
+        Page::extend(function ($pages) {
+
+            $pages->bindEvent('model.getAttribute', function ($attribute, $value) use ($pages) {
+
+                if ($attribute == 'content') {
+
+                    if (!count($pages->newModels) && !count($pages->additionals)) {
+
+                        $pages->newModels = [
+                            // 'formularios' => Form::class,
+                        ];
+
+                        $pages->additionals = [
+                            'posts' => Post::isPublished()->orderBy('published_at', 'DESC')->get(),
+                            'products' => Product::where('active', true)->get(),
+                            'pages' => Page::where('url', '<>', '/')->where('status', Page::STATUS_ACTIVE)->get(),
+                        ];
+
+                        $content = $pages->getContentAttribute();
+
+                        return $content;
+                    } else {
+
+                        return $value;
+                    }
+                }
+            });
+        });
     }
 }
